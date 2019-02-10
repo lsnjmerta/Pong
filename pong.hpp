b27cef4ca24158c6df65d3297a87c715af1ced47
Jakub Merta <jakubm@lsnova.pl>
2019-02-10 07:29:41 +0100

    Font pong hpp and utils

---
+++ pong.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Jakub Merta on 2019-02-10.
+//
+
+#ifndef GRA_PONG_H
+#define GRA_PONG_H
+
+#endif //GRA_PONG_H
---
+++ util.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Jakub Merta on 2019-02-10.
+//
+
+#ifndef GRA_UTIL_H
+#define GRA_UTIL_H
+
+#endif //GRA_UTIL_H
