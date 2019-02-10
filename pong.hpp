6baf468517b454d845648476ed394fd62e37354d
Jakub Merta <jakubm@lsnova.pl>
2019-02-10 07:34:15 +0100

    pong / definse constants and interface

--- pong.hpp
+++ pong.hpp
@@ -5,4 +5,21 @@
 #ifndef GRA_PONG_H
 #define GRA_PONG_H
 
+#include <SDL2/SDL.h>
+
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
+const int CENTER = 5;
+const float MAX_ANGLE = 3.142 /5.0;
+
+const float BALL_MAXSPEED = 8f;
+const float BALL_ACCELERATE = 1.05f;
+const float BALL_INIT_SPEED = 4.0f;
+const int BALL_WIDTH = 10;
+const int BALL_HEIGHT = 10;
+
+void Init(SDL_Renderer **ren, SDL_Window *win);
+void ClearAll(SDL_Renderer **ren, SDL_Window **win, SDL_GameController **controller);
+
 #endif //GRA_PONG_H
