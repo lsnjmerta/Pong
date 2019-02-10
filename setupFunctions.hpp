72788bf17913e21945fbd75e744af35524b99347
Jakub Merta <jakubm@lsnova.pl>
2019-02-10 09:46:48 +0100

    setup functions / fixes

--- setupFunctions.hpp
+++ setupFunctions.hpp
@@ -18,48 +18,47 @@ using namespace std;
 int getChoosenOption() {
     int mode = 1;
     int x, y;
-
     int menuWidth;
     int menuHeight;
-    SDL_Texture *menu = IMG_LoadTexture(ren, "images/menu.png");
-    SDL_QueryTexture(menu, NULL, NULL, &menuWidth, &menuHeight);
-    renderTexture(menu, ren, 0, 0, menuWidth, menuHeight);
-
     bool active = true;
 
-    cout << menuWidth << "   " << menuHeight;
+    SDL_Texture *menu = IMG_LoadTexture(ren, "images/menu.png");
 
 
     while (active) {
-        while(SDL_PollEvent(&mouse_event)){
-            if (mouse_event.type == SDL_MOUSEBUTTONDOWN){
+        SDL_QueryTexture(menu, NULL, NULL, &menuWidth, &menuHeight);
+        renderTexture(menu, ren, 0, 0, menuWidth, menuHeight);
+        SDL_RenderPresent(ren);
+
+        while (SDL_PollEvent(&mouse_event)) {
+            if (mouse_event.type == SDL_MOUSEBUTTONDOWN) {
                 x = mouse_event.motion.x;
                 y = mouse_event.motion.y;
 
                 cout << "click caught";
 
-                if (( x > 195) && (x < 410) && (y < 196) && (y > 93)){
+                if ((x > 195) && (x < 410) && (y < 196) && (y > 93)) {
                     mode = 1;
                     active = false;
                     cout << "selected solo";
                 }
 
-                if (( x > 195) && (x < 410) && (y < 312) && (y > 205)){
+                if ((x > 195) && (x < 410) && (y < 312) && (y > 205)) {
                     mode = 2;
                     active = false;
                     cout << "selected 2 players";
                 }
 
-                if (( x > 195) && (x < 410) && (y < 430) && (y > 336)){
+                if ((x > 195) && (x < 410) && (y < 430) && (y > 336)) {
                     mode = 3;
                     active = false;
                     cout << "quit";
                 }
             }
 
-            switch(mouse_event.type) {
+            switch (mouse_event.type) {
                 case SDL_KEYDOWN:
-                    if (mouse_event.key.keysym.sym == SDLK_ESCAPE){
+                    if (mouse_event.key.keysym.sym == SDLK_ESCAPE) {
                         mode = 3;
                         active = false;
                     }
