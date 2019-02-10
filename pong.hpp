81f06fe0ed393aff06d561a874b8586e36b82324
Jakub Merta <jakubm@lsnova.pl>
2019-02-10 08:31:25 +0100

    pong / removed controller support + fixes

--- pong.hpp
+++ pong.hpp
@@ -11,7 +11,7 @@ const int WIDTH = 640;
 const int HEIGHT = 480;
 
 const int CENTER = 5;
-const float MAX_ANGLE = 3.142 /5.0;
+const float MAX_ANGLE = 3.142f /5.0f;
 
 const float BALL_MAXSPEED = 8.0f;
 const float BALL_ACCELERATE = 1.05f;
@@ -20,7 +20,7 @@ const int BALL_WIDTH = 10;
 const int BALL_HEIGHT = 10;
 
 void InitSDL(SDL_Renderer **ren, SDL_Window **win);
-void ClearAll(SDL_Renderer **ren, SDL_Window **win, SDL_GameController **controller);
+void ClearAll(SDL_Renderer **ren, SDL_Window **win);
 
 
 #endif //GRA_PONG_H
